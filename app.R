1654dcee93d55384536cf4dd97140b9681877749
Hadley Wickham <h.wickham@gmail.com>
2024-07-23 12:24:09 -0500

    Improve story

--- app.R
+++ app.R
@@ -1,10 +1,39 @@
 library(shiny)
 
-generate_story <- function(noun, verb, adjective, adverb) {
-  paste0(
-    "Once upon a time, there was a ", adjective, " ", noun,
-     " who loved to ", verb, " ", adverb, ". It was the funniest thing ever!"
+story_templates <- list(
+)
+
+ui <- fluidPage(
+  titlePanel("Mad Libs Game featuring Joe Cheng and Winston Chang"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("noun", "Enter a noun:", ""),
+      textInput("noun2", "Enter another noun:", ""),
+      textInput("verb", "Enter a verb:", ""),
+      textInput("verb2", "Enter another verb:", ""),
+      textInput("adjective", "Enter an adjective:", ""),
+      textInput("adjective2", "Enter another adjective:", ""),
+      textInput("adverb", "Enter an adverb:", ""),
+      actionButton("submit", "Create Story")
+    ),
+    
+    mainPanel(
+      h3("Your Mad Libs Story:"),
+      textOutput("story")
+    )
   )
+)
+
+generate_story <- function(adjective, verb, adverb, noun, adjective2, noun2, verb2) {
+
+  glue::glue("
+    Once upon a time, Joe Cheng and Winston Chang were working on a
+    {adjective} Shiny app. They decided to {verb} it {adverb} so that everyone 
+    could enjoy using it. One day, Joe suggested they add a {noun} to the app, 
+    which made it even more {adjective2}. Winston agreed and also added a 
+    {noun2}. Together, they {verb2} and created the best Shiny app ever!
+  ")
 }
 
 ui <- fluidPage(
@@ -12,9 +41,12 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      textInput("noun1", "Enter a noun:", ""),
+      textInput("noun", "Enter a noun:", ""),
+      textInput("noun2", "Enter another noun:", ""),
       textInput("verb", "Enter a verb:", ""),
+      textInput("verb2", "Enter another verb:", ""),
       textInput("adjective", "Enter an adjective:", ""),
+      textInput("adjective2", "Enter another adjective:", ""),
       textInput("adverb", "Enter an adverb:", ""),
       actionButton("submit", "Create Story")
     ),
@@ -29,7 +61,15 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   story <- eventReactive(input$submit, {
-    generate_story(input$noun1, input$verb, input$adjective, input$adverb)
+    generate_story(    
+      noun = input$noun,
+      noun2 = input$noun2,
+      verb = input$verb,
+      verb2 = input$verb2,
+      adjective = input$adjective,
+      adjective2 = input$adjective2,
+      adverb = input$adverb
+)
   })
   
   output$story <- renderText({
